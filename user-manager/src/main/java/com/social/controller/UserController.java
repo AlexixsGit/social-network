4030de36e75e199dae5b18fae21c464d6d5c4c82
Edward Alexis Ortiz <alexixsortizz@gmail.com>
2020-09-21 11:31:00 -0500

    se crea el metodo update y se agregan metodos comunes

--- user-manager/src/main/java/com/social/controller/UserController.java
+++ user-manager/src/main/java/com/social/controller/UserController.java
@@ -3,9 +3,8 @@ package com.social.controller;
 import com.social.model.User;
 import com.social.service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @RequiredArgsConstructor
@@ -17,7 +16,26 @@ public class UserController {
     @PostMapping("/users")
     public Mono<User> save(@RequestBody User user) {
         return this.userService.save(user);
+    }
 
+    @GetMapping("/users/{id}")
+    public Mono<User> findById(@PathVariable String id) {
+        return this.userService.findById(id);
+    }
+
+    @GetMapping("/users")
+    public Flux<User> findAll() {
+        return this.userService.findAll();
+    }
+
+    @DeleteMapping("/users/{id}")
+    public Mono<Void> delete(@PathVariable String id) {
+        return this.userService.delete(id);
+    }
+
+    @PatchMapping("/users/{id}")
+    public Mono<User> update(@PathVariable String id, @RequestBody User user) {
+        return this.userService.update(id, user);
     }
 
 }
--- user-manager/src/main/java/com/social/service/UserService.java
+++ user-manager/src/main/java/com/social/service/UserService.java
@@ -1,9 +1,12 @@
 package com.social.service;
 
+import Util.Util;
 import com.social.model.User;
 import com.social.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @RequiredArgsConstructor
@@ -14,5 +17,24 @@ public class UserService {
 
     public Mono<User> save(User user) {
         return this.userRepository.save(user);
+    }
+
+    public Mono<User> findById(String id) {
+        return this.userRepository.findById(id);
+    }
+
+    public Flux<User> findAll() {
+        return this.userRepository.findAll();
+    }
+
+    public Mono<Void> delete(String id) {
+        return this.userRepository.deleteById(id);
+    }
+
+    public Mono<User> update(String id, User user) {
+        Mono<User> foundUser = this.findById(id);
+        foundUser = foundUser.doOnNext(currentUser ->
+                BeanUtils.copyProperties(user, currentUser, Util.getNullPropertyNames(user)));
+        return foundUser.flatMap(this.userRepository::save);
     }
 }
